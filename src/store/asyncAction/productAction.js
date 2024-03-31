672b5ba82de4ddaf91b101fcadb5296a33884d58
Vladimir <vladimir.abrahamyan96@gmail.com>
2024-03-31 21:50:57 +0400

    EasyMenu

--- src/store/asyncAction/productAction.js
+++ src/store/asyncAction/productAction.js
@@ -1,20 +1,18 @@
-import {createAsyncThunk} from '@reduxjs/toolkit'
-import data from './products.json'
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from "axios";
 
-// const baseURL = "https://easymenu-backend.vercel.app";
 const instance = axios.create({
     baseURL: "https://easymenu-backend.vercel.app",
 });
+
 export const fetchProductsByCategory = createAsyncThunk(
     "products/fetchProductsByCategory",
     async (categoryId, { rejectWithValue }) => {
         try {
-           let {data} =  await instance.get(`/products/${categoryId}`);
-
-            return data
+            const response = await instance.get(`/products/${categoryId}`);
+            return response.data;
         } catch (err) {
-            return rejectWithValue([], err);
+            return rejectWithValue({ data: [], error: err.message });
         }
     }
 );
@@ -23,13 +21,10 @@ export const fetchProductById = createAsyncThunk(
     "products/fetchProductById",
     async (productId, { rejectWithValue }) => {
         try {
-            let {data} =  await instance.get(`/product/${productId}`);
-            // if (!response.ok) {
-            //     throw new Error('Failed to fetch product by id');
-            // }
-            return data;
+            const response = await instance.get(`/product/${productId}`);
+            return response.data;
         } catch (err) {
-            return rejectWithValue(err.message);
+            return rejectWithValue({ data: null, error: err.message });
         }
     }
 );
